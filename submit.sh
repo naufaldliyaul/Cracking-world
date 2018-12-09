1c9ba5dcf6280943416988b9d4b673446855d797
Cripto <44128698+naufaldliyaul@users.noreply.github.com>
2018-12-09 16:57:43 +0700

    Add files via upload

---
+++ submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+echo " =================== Welcome To ===================";
+echo 'The  _____     _            _____           _     ';
+echo '    |  ___|_ _| | _____    |_   _|__   ___ | |___ ';
+echo '    | |_ / _` | |/ / _ \_____| |/ _ \ / _ \| / __|';
+echo '    |  _| (_| |   <  __/_____| | (_) | (_) | \__ \';
+echo '    |_|  \__,_|_|\_\___|     |_|\___/ \___/|_|___/ v.1.0';
+echo ' ---------------- Coded By bacot ---------------';
+echo '';
+read -p "Nick :" nick
+read -p "Team :" team
+read -p "File Name :" file
+echo "[+] Wait..."
+sleep 2
+echo "[+] File: data/$file"
+sleep 2
+echo "[+] Starting Submitter.."
+sleep 2
+defid_notifier $file $nick "$team"
